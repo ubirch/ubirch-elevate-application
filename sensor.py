2bc23c8d4a6c915dd43c15ca6821e303b6f72a0a
wowa <waldemar.gruenwald@ubirch.com>
2021-04-07 16:54:18 +0200

    simplified sensor data aggregation and fixed important bug of getting 33 instead of 32 measurements per interrupt

--- sensor.py
+++ sensor.py
@@ -1,5 +1,5 @@
 import pyboard
-import utime as time
+from pyboard.LIS2HH12 import FULL_SCALE_2G, ODR_100_HZ
 
 from sensor_config import *
 
@@ -30,29 +30,10 @@ class MovementSensor(object):
         # initialise the accelerometer
         self.pysense = pyboard.Pysense()
 
-        # taken from LIS2HH12.py
-        #   ARG => threshold
-        #   3 => max 8G; resolution: 125   micro G
-        #   2 => max 4G; resolution: 62.5  micro G
-        #   0 => max 2G; resolution: 31.25 micro G
-        self.pysense.accelerometer.set_full_scale(0) # CHECK: might be more readbale to use fs = self.pysense.accelerometer.FULL_SCALE_2G and set_full_scale(fs) (?)
+        self.pysense.accelerometer.set_full_scale(FULL_SCALE_2G) # => max 2G; resolution: 31.25 micro G
+
+        self.pysense.accelerometer.set_odr(ODR_100_HZ) # => 100 Hz; resolution: 80  milli seconds; max duration: 20400  ms
         
-
-        # taken from LIS2HH12.py
-        #   ARG => duration
-        #   0 => POWER DOWN
-        #   1 => 10  Hz; resolution: 800 milli seconds; max duration: 204000 ms # CHECK: what are the resolution and max duration values and where do they come from?
-        #   2 => 50  Hz; resolution: 160 milli seconds; max duration: 40800  ms
-        #   3 => 100 Hz; resolution: 80  milli seconds; max duration: 20400  ms
-        #   4 => 200 Hz; resolution: 40  milli seconds; max duration: 10200  ms
-        #   5 => 400 Hz; resolution: 20  milli seconds; max duration: 5100   ms
-        #   6 => 500 Hz; resolution: 10  milli seconds; max duration: 2550   ms
-        self.pysense.accelerometer.set_odr(3) # CHECK: might be more readbale to use odr = self.pysense.accelerometer.ODR_100_HZ and set_odr(odr) (?)
-        
-
-        # set highpass filter
-        # self.pysense.accelerometer.set_high_pass(1)
-
         # enable activity interrupt # CHECK: should say fifo instead of activity?
         print("start")
         self.pysense.accelerometer.restart_fifo()
@@ -67,31 +48,13 @@ class MovementSensor(object):
     # The accelerometer interrupt callback is triggered, when the fifo of the accelerometer is full.
     # It collects all data from the accelerometer and sets the trigger for the calculation.
     def accelerometer_interrupt_cb(self, pin):
-        self.pysense.accelerometer.enable_fifo_interrupt(handler=None) # CHECK: add comment that this actually disables the fifo interrupt handler
-        # print("*")
-        try:
-            accel_tuple = self.pysense.accelerometer.acceleration()
-            accel_list = list(accel_tuple)
-        except Exception as e:
-            print("ERROR      can't read data:", e)
-        # CHECK: code above can be replaced by accel_list = list() and extending the 'i' range() below by one on the negative side
+        self.pysense.accelerometer.enable_fifo_interrupt(handler=None) # disable the fifo interrupt handler
         # get data
         for i in range(32): # CHECK: I think in it's current version, this code reads 33 values, 32 here and 1 in the try/except block above
             try:
-                accel_tuple = self.pysense.accelerometer.acceleration()
-                accel_list.extend(accel_tuple) # CHECK: one could do the assignments to self.accel_xyz[i][0/1/2] = accel_tuple[0/1/2] 
-                                               # directly here, and remove the while loops below. (But might end up with self.accel_xyz
-                                               # being only partially filled with new values if there is an exception)
-                                               # also, append() might be better suited than extend() to not break up the tuple
+                self.accel_xyz[i] = self.pysense.accelerometer.acceleration()
             except Exception as e:
                 print("ERROR      can't read data:", e)
-        i = 0
-        while i < 32: # CHECK: this does not copy all 33 transfered values, but discards the last one (see comment after 'for i in range(32):' above)
-            j = 0
-            while j < 3:
-                self.accel_xyz[i][j] = accel_list[i * 3 + j]
-                j += 1
-            i += 1
 
         self.pysense.accelerometer.restart_fifo()
         self.pysense.accelerometer.enable_fifo_interrupt(self.accelerometer_interrupt_cb) # CHECK: re-enabling the interrupt here will overwrite data in accel_xyz
